ad2c8b1213afd48070dfb8a96b8004916cbbaace
eduardo <jose.eduardo@inbec.edu.br>
2024-08-22 17:08:29 -0300

    test: :test_tube: tests for class PhraseAnalyzer

---
+++ tests/core/PhraseAnalyzer.test.ts
@@ -0,0 +1,36 @@
+import { PhraseAnalyzer } from '../../src/core/PhraseAnalyzer';
+import { TreeNode } from '../../src/core/TreeNode';
+
+describe('Testes dos métodos da classe PhraseAnalyzer', () => {
+    let phraseAnalyzer: PhraseAnalyzer;
+
+    const hierarchy: TreeNode = {
+        Animais: {
+            'Mamíferos': {
+                Felinos: ['Leões', 'Tigres']
+            },
+            Aves: {
+                'Pássaros': ['Papagaios', 'Canários'],
+            },
+        },
+    };
+
+    beforeEach(() => {
+        phraseAnalyzer = new PhraseAnalyzer(hierarchy);
+    });
+
+    test('Deve encontrar uma correspondência no nível de especifidade buscado', () => {
+        const result = phraseAnalyzer.analyze('Eu amo papagaios', 2);
+        expect(result).toBe('Aves = 1;');
+    });
+
+    test('Deve encontrar duas correspondências no nível de especifidade buscado', () => {
+        const result = phraseAnalyzer.analyze('Eu vi leões e canários', 3);
+        expect(result).toBe('Felinos = 1; Pássaros = 1;');
+    });
+
+    test('Deve retornar um resultado vazio quando nenhuma palavra for encontrada em nenhum nível', () => {
+        const result = phraseAnalyzer.analyze('Eu gosto de sorvete', 2);
+        expect(result).toBe('');
+    });
+});
